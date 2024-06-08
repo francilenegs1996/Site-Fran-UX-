7be128eddb4b1b64f2cd7fc6a0049b02015b1083
francilenegs1996 <136385190+francilenegs1996@users.noreply.github.com>
2024-06-08 16:28:40 -0300

    meu primeiro commit

--- src/page/SinglePage.tsx
+++ src/page/SinglePage.tsx
@@ -1,11 +1,32 @@
-import './style.css';
-
+import "./style.css";
+import logo from "../assets/Logofran.svg";
 
 export default function SinglePage() {
-		return (
-			<div className='conteudoTodo'>
-				
-			</div>
-
-		)
+  return (
+    <div className="conteudoTodo">
+        <header>
+          <nav className="navbar">
+            <div className="logo">
+              <a href="# observacão, o # é um link">
+                <img className="logofran" src={logo} alt="Logo Fran UX"></img>
+              </a>
+            </div>
+            <ul className="nav-links">
+              <li>
+                <a href="#">HOME</a>
+              </li>
+              <li>
+                <a href="#">PORTFÓLIO</a>
+              </li>
+              <li>
+                <a href="#">SERVIÇOS</a>
+              </li>
+              <li>
+                <a href="#">CONTATO</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+    </div>
+  );
 }
