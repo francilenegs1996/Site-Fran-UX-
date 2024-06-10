8aad9f510b40a570808b1df82265cdc7c3cb8993
francilenegs1996 <136385190+francilenegs1996@users.noreply.github.com>
2024-06-10 18:14:05 -0300

    Construção da seção principal

--- src/page/SinglePage.tsx
+++ src/page/SinglePage.tsx
@@ -1,10 +1,13 @@
 import "./style.css";
 import Nav from "../components/Nav/Nav";
+import Sectionmain from "../components/Sectionmain/Sectionmain";
+
 
 export default function SinglePage() {
   return (
     <div className="conteudoTodo">
-        <Nav />
+      <Nav />
+      <Sectionmain />
     </div>
   );
 }
