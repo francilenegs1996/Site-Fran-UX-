3b3bf630b29ba3b9528b89e55690c9d5dbfbde37
francilenegs1996 <136385190+francilenegs1996@users.noreply.github.com>
2024-06-10 15:15:58 -0300

    menu burguer

--- src/page/SinglePage.tsx
+++ src/page/SinglePage.tsx
@@ -1,36 +1,10 @@
 import "./style.css";
-import logo from "../assets/Logofran.svg";
+import Nav from "../components/Nav/Nav";
 
 export default function SinglePage() {
   return (
     <div className="conteudoTodo">
-        <header>
-          <nav className="navbar">
-            <div className="logo">
-              <a href="# observacão, o # é um link">
-                <img className="logofran" src={logo} alt="Logo Fran UX"></img>
-              </a>
-            </div>
-            <ul className="nav-links">
-              <li>
-                <a className="nav-button" href="#">HOME</a>
-                <div></div>
-              </li>
-              <li>
-                <a className="nav-button" href="#">PORTFÓLIO</a>
-                <div></div>
-              </li>
-              <li>
-                <a className="nav-button" href="#">SERVIÇOS</a>
-                <div></div>
-              </li>
-              <li>
-                <a className="nav-button" href="#">CONTATO</a>
-                <div></div>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Nav />
     </div>
   );
 }
