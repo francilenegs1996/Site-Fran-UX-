30b85217c74443ef49fe42fd4b560f69eb9bcac0
francilenegs1996 <136385190+francilenegs1996@users.noreply.github.com>
2024-06-19 11:55:07 -0300

    novo layout

---
+++ src/App/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+
+
+export const Container = styled.main`
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: row;
+background-color: '#030202';
+
+
+@media (max-width: 720px) {
+    flex-direction: column-reverse;
+}
+
+
+`;
+
---
+++ src/components/ContainerPage/ContainerPage.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+
+
+export const ContainerPage = styled.div`
+width: 90vw;
+height: 100%;
+background-color: #0000;
+display: flex;
+align-items: center;
+justify-content: center;
+
+
+@media (max-width: 720px) {
+    width: 100vw;
+}
+`
